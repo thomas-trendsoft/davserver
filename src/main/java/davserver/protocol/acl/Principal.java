ba0923af21335433fc9ee5bc2a289549d298e4b4
Thomas Krieger <thomas@trend-soft.de>
2018-10-28 02:09:30 +0100

    acl prop refactor

--- src/main/java/davserver/protocol/acl/Principal.java
+++ src/main/java/davserver/protocol/acl/Principal.java
@@ -2,9 +2,25 @@ package davserver.protocol.acl;
 
 import org.w3c.dom.Element;
 
+import davserver.protocol.acl.properties.AlternateURISet;
 import davserver.protocol.xml.DAVXMLObject;
+import davserver.repository.Property;
 
+/**
+ * ACL Principal Implementation
+ * 
+ * @author tkrieger
+ *
+ */
 public class Principal extends DAVXMLObject {
+
+	private AlternateURISet alternateURIset;
+	
+	private Property principalURL;
+	
+	public AlternateURISet getAlternateURIset() {
+		return alternateURIset;
+	}
 
 	@Override
 	public Element appendXML(Element root) {
